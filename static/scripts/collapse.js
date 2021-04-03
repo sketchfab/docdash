b338b1f67f4243b44787a0ff22f8b47cf61a6558
Arthur Jamain <arthur.jamain@gmail.com>
2021-04-03 14:13:45 +0200

    Adds nav highlight on scroll + search only in current version

--- static/scripts/collapse.js
+++ static/scripts/collapse.js
@@ -1,15 +1,28 @@
-function initializeFoldableMenus() {
-  var foldables = document.querySelectorAll(".foldable");
-  for (var f in foldables) {
-    if (foldables.hasOwnProperty(f)) {
-      foldables[f].addEventListener("click", (e) => {
-        if (e.target.parentElement.dataset.type !== "method") {
-          e.currentTarget.classList.toggle("unfolded");
-          e.preventDefault();
-        }
-      });
-    }
+const nav = document.querySelector("nav");
+
+function openCurrentMenu() {
+  if (currentVersion) {
+    const version = document.querySelector(
+      "." + currentVersion[0].replace(/\./gi, "_")
+    );
+    version.classList.add("unfolded");
+    nav.scrollTo(0, version.offsetTop);
   }
+}
+
+function initializeFoldableMenus() {
+  openCurrentMenu();
+  document.querySelectorAll(".foldable").forEach((e) =>
+    e.addEventListener("click", (e) => {
+      if (
+        e.target.parentElement.dataset.type !== "method" &&
+        e.currentTarget.classList.contains("unfolded")
+      ) {
+        e.currentTarget.classList.remove("unfolded");
+        e.preventDefault();
+      }
+    })
+  );
 }
 
 initializeFoldableMenus();
--- static/scripts/search.js
+++ static/scripts/search.js
@@ -1,99 +1,122 @@
 var searchAttr = "data-search-mode";
+var searchField = document.getElementById("nav-search");
 function contains(a, m) {
   return (a.textContent || a.innerText || "").toUpperCase().indexOf(m) !== -1;
 }
 
 //on search
-document
-  .getElementById("nav-search")
-  .addEventListener("keyup", function (event) {
-    var search = this.value.toUpperCase();
+searchField.addEventListener("keyup", function (event) {
+  var search = this.value.toUpperCase();
 
-    if (!search) {
-      //no search, show all results
-      document.documentElement.removeAttribute(searchAttr);
+  if (!search) {
+    //no search, show all results
+    document.documentElement.removeAttribute(searchAttr);
 
+    if (currentVersion) {
+      document.querySelectorAll(".foldable").forEach(function (el) {
+        el.classList.remove("hidden");
+      });
+    } else {
       document.querySelectorAll(".foldable").forEach(function (el) {
         el.classList.remove("unfoldable");
       });
-      document
-        .querySelectorAll("nav > ul > li:not(.level-hide)")
-        .forEach(function (elem) {
-          elem.style.display = "block";
-        });
+    }
 
-      if (typeof hideAllButCurrent === "function") {
-        //let's do what ever collapse wants to do
-        hideAllButCurrent();
-      } else {
-        //menu by default should be opened
-        document
-          .querySelectorAll("nav > ul > li > ul li")
-          .forEach(function (elem) {
-            elem.style.display = "block";
-          });
-      }
-    } else {
-      //we are searching
-      document.documentElement.setAttribute(searchAttr, "");
+    history.replaceState({}, "", location.origin + location.pathname);
 
-      document.querySelectorAll(".foldable").forEach(function (el) {
-        el.classList.add("unfoldable");
-      });
-
-      //show all parents
-      document.querySelectorAll("nav > ul > li").forEach(function (elem) {
+    document
+      .querySelectorAll("nav > ul > li:not(.level-hide)")
+      .forEach(function (elem) {
         elem.style.display = "block";
       });
-      //hide all results
+
+    if (typeof hideAllButCurrent === "function") {
+      //let's do what ever collapse wants to do
+      hideAllButCurrent();
+    } else {
+      //menu by default should be opened
       document
         .querySelectorAll("nav > ul > li > ul li")
         .forEach(function (elem) {
-          elem.style.display = "none";
+          elem.style.display = "block";
         });
-      //show results matching filter
+    }
+  } else {
+    //we are searching
+    document.documentElement.setAttribute(searchAttr, "");
+
+    if (currentVersion) {
       document
-        .querySelectorAll("nav > ul > li > ul a")
-        .forEach(function (elem) {
-          if (!contains(elem.parentNode, search)) {
-            return;
-          }
-          elem.parentNode.style.display = "block";
+        .querySelectorAll(".foldable:not(." + currentVersionClass + ")")
+        .forEach(function (el) {
+          el.classList.add("hidden");
         });
-      //hide parents without children
-      document.querySelectorAll("nav > ul > li").forEach(function (parent) {
-        var countSearchA = 0;
-        parent.querySelectorAll("a").forEach(function (elem) {
-          if (contains(elem, search)) {
-            countSearchA++;
-          }
-        });
-
-        var countUl = 0;
-        var countUlVisible = 0;
-        parent.querySelectorAll("ul").forEach(function (ulP) {
-          // count all elements that match the search
-          if (contains(ulP, search)) {
-            countUl++;
-          }
-
-          // count all visible elements
-          var children = ulP.children;
-          for (i = 0; i < children.length; i++) {
-            var elem = children[i];
-            if (elem.style.display != "none") {
-              countUlVisible++;
-            }
-          }
-        });
-
-        if (countSearchA == 0 && countUl === 0) {
-          //has no child at all and does not contain text
-          parent.style.display = "none";
-        } else if (countSearchA == 0 && countUlVisible == 0) {
-          //has no visible child and does not contain text
-          parent.style.display = "none";
-        }
+    } else {
+      document.querySelectorAll(".foldable").forEach(function (el) {
+        el.classList.add("unfoldable");
       });
     }
-  });
+
+    history.replaceState(
+      {},
+      "",
+      location.origin + location.pathname + "?q=" + this.value
+    );
+
+    //show all parents
+    document.querySelectorAll("nav > ul > li").forEach(function (elem) {
+      elem.style.display = "block";
+    });
+    //hide all results
+    document.querySelectorAll("nav > ul > li > ul li").forEach(function (elem) {
+      elem.style.display = "none";
+    });
+    //show results matching filter
+    document.querySelectorAll("nav > ul > li > ul a").forEach(function (elem) {
+      if (!contains(elem.parentNode, search)) {
+        return;
+      }
+      elem.parentNode.style.display = "block";
+    });
+    //hide parents without children
+    document.querySelectorAll("nav > ul > li").forEach(function (parent) {
+      var countSearchA = 0;
+      parent.querySelectorAll("a").forEach(function (elem) {
+        if (contains(elem, search)) {
+          countSearchA++;
+        }
+      });
+
+      var countUl = 0;
+      var countUlVisible = 0;
+      parent.querySelectorAll("ul").forEach(function (ulP) {
+        // count all elements that match the search
+        if (contains(ulP, search)) {
+          countUl++;
+        }
+
+        // count all visible elements
+        var children = ulP.children;
+        for (i = 0; i < children.length; i++) {
+          var elem = children[i];
+          if (elem.style.display != "none") {
+            countUlVisible++;
+          }
+        }
+      });
+
+      if (countSearchA == 0 && countUl === 0) {
+        //has no child at all and does not contain text
+        parent.style.display = "none";
+      } else if (countSearchA == 0 && countUlVisible == 0) {
+        //has no visible child and does not contain text
+        parent.style.display = "none";
+      }
+    });
+  }
+});
+
+if (initialSearch) {
+  searchField.value = initialSearch;
+  searchField.dispatchEvent(new Event("keyup"));
+}
